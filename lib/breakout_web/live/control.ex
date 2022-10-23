0ddb03ae8c9bcbb2ccbb34ff7b1f7a26fe9f5b74
Daniel Rank <dwrank@gmail.com>
2022-10-23 13:26:56 -0700

    Move paddle immediately to the cursor

--- lib/breakout_web/live/control.ex
+++ lib/breakout_web/live/control.ex
@@ -28,11 +28,9 @@ defmodule BreakoutWeb.Live.Control do
 
   @spec cursor_move_paddle(paddle(), cursor()) :: paddle()
   def cursor_move_paddle(paddle, cursor) do
-    margin = 5
-
     cond do
-      cursor.x < paddle.left + paddle.half_width - margin ||
-      cursor.x > paddle.left + paddle.half_width + margin ->
+      cursor.x < paddle.left + paddle.half_width ||
+      cursor.x > paddle.left + paddle.half_width ->
         %{paddle | direction: :to_cursor}
       true -> paddle
     end
@@ -43,52 +41,19 @@ defmodule BreakoutWeb.Live.Control do
   @spec move_paddle_left(paddle(), number()) :: paddle()
   def move_paddle_left(paddle, unit) do
     new_left = max(unit, paddle.left - paddle.speed)
-
     %{paddle | left: new_left, right: paddle.right - (paddle.left - new_left)}
   end
 
   @spec move_paddle_right(paddle(), number()) :: paddle()
   def move_paddle_right(paddle, unit) do
     new_left = min(paddle.left + paddle.speed, unit * (@board_cols - paddle.length - 1))
-
     %{paddle | left: new_left, right: paddle.right + (new_left - paddle.left)}
   end
 
   @spec move_paddle_to_cursor(paddle(), cursor(), number()) :: paddle()
   def move_paddle_to_cursor(paddle, cursor, unit) do
-    new_left = cursor.x - paddle.half_width
-
-    cond do
-      new_left < paddle.left ->
-        move_paddle_left_to_cursor(paddle, new_left, unit)
-      new_left > paddle.left ->
-        move_paddle_right_to_cursor(paddle, new_left, unit)
-      true ->
-        %{paddle | direction: :stationary}
-    end
-  end
-
-  @spec move_paddle_left_to_cursor(paddle(), number(), number()) :: paddle()
-  defp move_paddle_left_to_cursor(paddle, new_left, unit) do
-    paddle = move_paddle_left(paddle, unit)
-
-    cond do
-      new_left >= paddle.left ->
-        paddle = %{paddle | direction: :stationary}
-      true ->
-        paddle
-    end
-  end
-
-  @spec move_paddle_right_to_cursor(paddle(), number(), number()) :: paddle()
-  defp move_paddle_right_to_cursor(paddle, new_left, unit) do
-    paddle = move_paddle_right(paddle, unit)
-
-    cond do
-      new_left <= paddle.left ->
-        paddle = %{paddle | direction: :stationary}
-      true ->
-        paddle
-    end
+    new_left = min(cursor.x - paddle.half_width, unit * (@board_cols - paddle.length - 1))
+    new_left = max(unit, new_left)
+    %{paddle | direction: :stationary, left: new_left, right: paddle.right + (new_left - paddle.left)}
   end
 end
